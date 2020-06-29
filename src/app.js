5083ba81ea4b885a5500565cff02d1fbbf7fe1f2
Tobechi <tobechichukwuleta125@gmail.com>
2020-06-29 14:42:03 -0700

    Setup for Heroku

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-const port = process.env.PORT || 3000
+const portt = process.env.PORT || 3000
 //Define paths for express config
 const pathe = path.join(__dirname, '../public')
 const viewPath = path.join(__dirname, '../templates/views')
@@ -92,6 +92,6 @@ app.get('*', (request, response) => {
 })
 
 
-app.listen(port, () => {
-    console.log('Server is up on port' + port)
+app.listen(portt, () => {
+    console.log('Server is up on port' + portt)
 })
